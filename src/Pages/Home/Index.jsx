dc29e615fa5e5b1ef3c48549d132182674e03c5a
Anshul Sharma <anshul.sharma6163@gmail.com>
2024-05-12 21:23:46 +0530

    Product section add with couple of changes in css

--- src/Pages/Home/Index.jsx
+++ src/Pages/Home/Index.jsx
@@ -1,16 +1,60 @@
-import React from 'react'
-import Homeslider from './Slider/Homeslider'
-import Catslider from '../../component/categary slider/Catslider'
-import Banner from '../../component/Banner/Banner'
+import React from "react";
+import Homeslider from "./Slider/Homeslider";
+import Catslider from "../../component/categary slider/Catslider";
+import Banner from "../../component/Banner/Banner";
+import {Link} from "react-router-dom";
+import "./index.css"
 
 const Home = () => {
+
+  const filters = [
+    "All",
+    "Milks & Dairies",
+    "Coffee & Teas",
+    "Pet Foods ",
+    "Meats",
+    "Vegetables",
+    "Fruits"
+  ]
+
   return (
     <>
-      <Homeslider/>
-      <Catslider/>
-      <Banner/>
-    </>
-  )
-}
+      <Homeslider />
+      <Catslider />
+      <Banner />
 
-export default Home
+      <section className="w-full p-11">
+        <div className="w-full">
+          <div className="flex items-center justify-between">
+            <h2 className="text-4xl font-semibold">Popular Products</h2>
+            <div className=" h-auto w-auto flex items-center justify-center">
+              <ul className="filterTab flex gap-3 ">
+
+                <Link><li>All</li></Link>
+                <Link><li>Milks & Dairies</li></Link>
+                <Link><li>Coffee & Teas </li></Link>
+                <Link><li>Pet Foods </li></Link>
+                <Link><li>Meats </li></Link>
+                <Link><li>Vegetables</li></Link>
+                <Link><li>Fruits</li></Link>
+
+                {/* {
+                  filters.map((item,index) => {
+                    return (
+                        <Link><li className="hover:text-[#319768] transition-all text-md font-semibold opacity-80" key={index}>{item}</li></Link>
+                    )
+                  })
+                } */}
+              </ul>
+            </div>
+          </div>
+
+
+          <div className=""></div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/component/Product/Product.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Product.css'
+
+const Product = () => {
+  return (
+    <div className='productBox'>Product</div>
+  )
+}
+
+export default Product
